4ead898d89bc7c8209da8252e827c8404d8f63d3
David Ross <daveross858@gmail.com>
2025-09-22 00:20:22 -0700

    Remove history, settings, and weekly pages
    Deleted the /history, /settings, and /weekly pages and their related static assets and manifest entries. Updated .env.local to enable Firebase and use production Firebase project configuration.

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -23,21 +23,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/history": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/history.js"
-    ],
-    "/settings": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/settings.js"
-    ],
-    "/weekly": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/weekly.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0ff54b2f15513df3"; }
+/******/ 		__webpack_require__.h = function() { return "d6ff759a773516b3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
