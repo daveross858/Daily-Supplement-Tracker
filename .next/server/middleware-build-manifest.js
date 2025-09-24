249fff9e8a5ae8530443a879c6cf621d2c5c4376
David Ross <daveross858@gmail.com>
2025-09-24 16:52:16 -0700

    Update build for /library route and manifest changes
    Replaces the /settings route with /library in build and middleware manifests, updates the build manifest and sorted pages, and adjusts the pages-manifest.json ordering. Includes updated webpack build artifacts.

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -24,10 +24,10 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/settings": [
+    "/library": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
-      "static/chunks/pages/settings.js"
+      "static/chunks/pages/library.js"
     ]
   },
   "ampFirstPages": []
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8202fbeda0360fd8"; }
+/******/ 		__webpack_require__.h = function() { return "14d4f12557b2ddfb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
