f1456958d47af44da99bcc7ee7b3e09183a3d08f
David Ross <daveross858@gmail.com>
2025-09-24 14:36:13 -0700

    Add /weekly page and update build artifacts
    Introduces a new /weekly page to the Next.js build, updating build-manifest, pages-manifest, and related server/static artifacts. This includes new and updated server and client bundles, as well as hot-update files to support the new route.

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e6449ec7c3a3c458"; }
+/******/ 		__webpack_require__.h = function() { return "3fcaf174642aaf51"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
